0e431b1b224ab68c0a0a5708085025a43d0117ff
naveen-sha <naveen972821@gmail.com>
2026-05-04 21:41:40 +0530

    re-view code

--- editor.js
+++ editor.js
@@ -92,12 +92,4 @@ Object.keys(filters).forEach(key =>{
         document.querySelector(".image").style.filter=generatefilterstring();
     })}
 })
-function generatefilterstring(){    let filterstring="";
-    Object.keys(filters).forEach(key =>{
-        const value=document.getElementById(key).value;
-        filterstring+=`${key}(${value}${filters[key].unit}) `;
-    })
-    return filterstring;
-}
-
 
