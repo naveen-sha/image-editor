1dceac881e3ae5dbfcb302c102e853d0bf2b07d3
naveen-sha <naveen972821@gmail.com>
2026-05-20 22:20:45 +0530

    start featuring with js

---
+++ editor.js
@@ -0,0 +1,15 @@
+const filters ={
+    brightness: 100,
+    contrast: 100,
+    saturation: 100,
+    exposure: 100,
+    hueRotation: 0,
+    blur: 0,
+    graysclale: 0,
+    invert: 0,
+    sepia: 0,
+    opacity: 100,
+    invert: 0,
+
+
+}
